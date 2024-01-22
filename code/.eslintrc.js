cc8812be1d7dc1d6d51c80b9b4a1d2c5189b2092
Dany Reyna <luisd.reyna@outlook.com>
2024-01-21 19:00:25 -0600

    build: add result of `ng add @angular-eslint/schematics` and merge with existing eslint config

--- code/.eslintrc.js
+++ code/.eslintrc.js
@@ -1,34 +1,93 @@
-module.exports = {
+function getExtendsArrayWithPrettier(...extendsItems) {
+  /*
+   * "prettier" has to be last, so it gets the chance to override other configs
+   */
+  return [...extendsItems, "prettier"];
+}
+
+function getBaseTypeScriptConfig(...extendsItems) {
+  return {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: getExtendsArrayWithPrettier(
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      ...extendsItems,
+    ),
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: ["@typescript-eslint"],
+  };
+}
+
+const esLintOverride = {
+  files: [".eslintrc.{js,cjs}"],
   env: {
-    browser: true,
-    es2021: true,
+    node: true,
+    es2023: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:jest-dom/recommended",
-    "prettier",
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-    {
-      files: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
-      extends: ["plugin:testing-library/angular"],
-    },
-  ],
-  parser: "@typescript-eslint/parser",
+  extends: getExtendsArrayWithPrettier("eslint:recommended"),
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "script",
   },
-  plugins: ["@typescript-eslint"],
+};
+
+const angularOverride = {
+  files: ["*.ts"],
+  ...getBaseTypeScriptConfig(
+    "plugin:@angular-eslint/recommended",
+    "plugin:@angular-eslint/template/process-inline-templates",
+  ),
+  rules: {
+    "@angular-eslint/directive-selector": [
+      "error",
+      {
+        type: "attribute",
+        prefix: "app",
+        style: "camelCase",
+      },
+    ],
+    "@angular-eslint/component-selector": [
+      "error",
+      {
+        type: "element",
+        prefix: "app",
+        style: "kebab-case",
+      },
+    ],
+  },
+};
+
+const angularTestOverride = {
+  files: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+  ...getBaseTypeScriptConfig(
+    "plugin:jest-dom/recommended",
+    "plugin:testing-library/angular",
+  ),
+};
+
+const angularHtmlOverride = {
+  files: ["*.html"],
+  extends: getExtendsArrayWithPrettier(
+    "plugin:@angular-eslint/template/recommended",
+    "plugin:@angular-eslint/template/accessibility",
+  ),
   rules: {},
 };
+
+const finalConfig = {
+  root: true,
+  overrides: [
+    esLintOverride,
+    angularOverride,
+    angularTestOverride,
+    angularHtmlOverride,
+  ],
+};
+
+module.exports = finalConfig;
