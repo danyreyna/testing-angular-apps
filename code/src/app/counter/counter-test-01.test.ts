ec9abe823b2b094dbe81994a189cb1bbd374567b
Dany Reyna <luisd.reyna@outlook.com>
2024-01-23 19:22:26 -0600

    feat: add `test-01` content

---
+++ code/src/app/counter/counter-test-01.test.ts
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/angular";
+import { userEvent } from "@testing-library/user-event";
+import { expect, test } from "vitest";
+import { CounterComponent } from "./counter.component";
+
+test("should increment the counter on click", async () => {
+  await render(CounterComponent);
+
+  const increment = screen.getByRole("button", { name: /increment/i });
+  const decrement = screen.getByRole("button", { name: /decrement/i });
+  const message = screen.getByText(/current count/i);
+
+  expect(message).toHaveTextContent("Current count: 0");
+  await userEvent.click(increment);
+  expect(message).toHaveTextContent("Current count: 1");
+  await userEvent.click(decrement);
+  expect(message).toHaveTextContent("Current count: 0");
+});
