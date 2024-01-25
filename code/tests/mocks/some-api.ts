6e3e31605a69ce4ce96d0e97c2510d67fb450884
Dany Reyna <luisd.reyna@outlook.com>
2024-01-24 21:20:14 -0600

    test(mocks): add mock request handlers for `some-api.ts`

---
+++ code/tests/mocks/some-api.ts
@@ -0,0 +1,37 @@
+import { http, type HttpHandler, HttpResponse } from "msw";
+import { PathParams } from "msw/lib/core/utils/matching/matchRequestUrl";
+
+const { json } = HttpResponse;
+
+type LoginResponse = { username: string } | { message: string };
+
+export const handlers: Array<HttpHandler> = [
+  http.post<PathParams, { username: string; password: string }, LoginResponse>(
+    "https://auth-provider.example.com/api/login",
+    async ({ request }) => {
+      const body = await request.json();
+
+      if (!body.password) {
+        return json(
+          {
+            message: "password required",
+          },
+          { status: 400 },
+        );
+      }
+
+      if (!body.username) {
+        return json(
+          {
+            message: "username required",
+          },
+          { status: 400 },
+        );
+      }
+
+      return json({
+        username: body.username,
+      });
+    },
+  ),
+];
