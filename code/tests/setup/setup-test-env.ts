df9a20081be46e39431ec8f65ce07e6bd3b727ad
Dany Reyna <luisd.reyna@outlook.com>
2024-01-24 21:23:01 -0600

    build(setup-test-env.ts): add `TextEncoder` to `globalThis` since happy-dom does not include it by default

--- code/tests/setup/setup-test-env.ts
+++ code/tests/setup/setup-test-env.ts
@@ -5,6 +5,9 @@ import {
   platformBrowserDynamicTesting,
 } from "@angular/platform-browser-dynamic/testing";
 import "@testing-library/jest-dom/vitest";
+import { TextEncoder } from "node:util";
+
+globalThis.TextEncoder = TextEncoder;
 
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
