1d8f7fbe851cc514cb1f2fe39e63ba1c6c7013ee
Dany Reyna <luisd.reyna@outlook.com>
2024-01-25 21:58:15 -0600

    test(login-reactive): fix test name

--- code/src/app/login-reactive/login-reactive-test-03.test.ts
+++ code/src/app/login-reactive/login-reactive-test-03.test.ts
@@ -15,7 +15,7 @@ const buildLoginForm = build<LoginFormValues>({
   },
 });
 
-test("submitting the form calls onSubmit with username and password", async () => {
+test("submitting the form displays a welcome message", async () => {
   await render(LoginReactiveComponent);
   const { username, password } = buildLoginForm.one();
 
