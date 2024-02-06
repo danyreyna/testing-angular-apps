f30cc3823d569caddcd97015c80325f15a779376
Dany Reyna <luisd.reyna@outlook.com>
2024-02-06 09:30:34 -0600

    refactor(handle-http-error): log error first

--- code/src/app/common/handle-http-error.ts
+++ code/src/app/common/handle-http-error.ts
@@ -2,6 +2,10 @@ import { type HttpErrorResponse } from "@angular/common/http";
 import { throwError } from "rxjs";
 
 export function handleHttpError(errorResponse: HttpErrorResponse) {
+  // in a real world app, we may send the error to some remote logging infrastructure
+  // instead of just logging it to the console
+  console.error(errorResponse);
+
   const isNetworkError =
     errorResponse.error instanceof ProgressEvent && errorResponse.status === 0;
 
@@ -9,10 +13,6 @@ export function handleHttpError(errorResponse: HttpErrorResponse) {
     ? "A network error ocurred"
     : errorResponse.error?.message ??
       `Backend returned code ${errorResponse.status}: ${errorResponse.message}`;
-
-  // in a real world app, we may send the error to some remote logging infrastructure
-  // instead of just logging it to the console
-  console.error(errorResponse);
 
   return throwError(() => ({ message, status: errorResponse.status }));
 }
