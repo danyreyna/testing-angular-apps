cbe6b0620234118dc27d20c95988fa0c3661af20
Dany Reyna <luisd.reyna@outlook.com>
2024-02-11 20:00:06 -0600

    refactor(code/tests/setup/setup-test-env.ts): set up angular test bed first

--- code/tests/setup/setup-test-env.ts
+++ code/tests/setup/setup-test-env.ts
@@ -8,6 +8,11 @@ import "@testing-library/jest-dom/vitest";
 import { afterAll, afterEach, beforeAll } from "vitest";
 import { server } from "../mocks/index";
 
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+);
+
 beforeAll(() => {
   server.listen();
 });
@@ -17,8 +22,3 @@ afterAll(() => {
 afterEach(() => {
   server.resetHandlers();
 });
-
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
-);
