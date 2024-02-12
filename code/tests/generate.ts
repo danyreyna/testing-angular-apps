1f0689b7396366f9a1b052b00a3c7caabe5545e8
Dany Reyna <luisd.reyna@outlook.com>
2024-02-12 00:58:40 -0600

    refactor(code/tests/generate.ts): use correct maker method

--- code/tests/generate.ts
+++ code/tests/generate.ts
@@ -4,7 +4,7 @@ import type { User } from "../src/app/common/user";
 
 export const buildUser = build<User>({
   fields: {
-    id: perBuild(() => faker.datatype.uuid()),
+    id: perBuild(() => faker.string.uuid()),
     username: perBuild(() => faker.internet.userName()),
     password: perBuild(() => faker.internet.password()),
   },
