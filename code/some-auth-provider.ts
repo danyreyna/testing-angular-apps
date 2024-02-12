04dc98e63f4a6d0627f1953e79a72a4b0344ff2d
Dany Reyna <luisd.reyna@outlook.com>
2024-02-11 19:59:17 -0600

    feat(code): add simulated auth provider module

---
+++ code/some-auth-provider.ts
@@ -0,0 +1,16 @@
+/*
+ * Pretend this is firebase, netlify, or auth0's code.
+ * You shouldn't have to implement something like this in your own app.
+ */
+
+export async function login() {
+  console.log("Log in");
+}
+
+export async function register() {
+  console.log("Register");
+}
+
+export async function logout() {
+  console.log("Log out");
+}
