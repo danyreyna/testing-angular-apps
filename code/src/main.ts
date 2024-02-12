7b1f8f12e915d2f1e3e11d772060a8614ae7cc1a
Dany Reyna <luisd.reyna@outlook.com>
2024-02-11 21:40:02 -0600

    feat(code/src/main.ts): call `enableDebugTools` to use the built-in angular profiler

--- code/src/main.ts
+++ code/src/main.ts
@@ -1,7 +1,22 @@
-import { bootstrapApplication } from "@angular/platform-browser";
+import {
+  bootstrapApplication,
+  enableDebugTools,
+} from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { appConfig } from "./app/app.config";
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err),
-);
+bootstrapApplication(AppComponent, appConfig)
+  .then((applicationRef) => {
+    /*
+     * By enabling this, you can call `ng.profiler.timeChangeDetection()` in the browser's console.
+     * It tells Angular's change detection to run for about 500ms.
+     * Then it prints an object with:
+     * `numTicks`, how many times Angular triggered change detection.
+     * `msPerTick`, the average time change detection took.
+     * Tip: Don't let the change detection cycle go beyond 10ms.
+     */
+
+    const [componentRef] = applicationRef.components;
+    enableDebugTools(componentRef);
+  })
+  .catch((err) => console.error(err));
