ed3cb7b1954ca654ac4dfcc813cd3133827bbfae
Dany Reyna <luisd.reyna@outlook.com>
2024-02-11 21:17:16 -0600

    feat(code/tests/setup/setup-test-env.ts): add timers cleanup

--- code/tests/setup/setup-test-env.ts
+++ code/tests/setup/setup-test-env.ts
@@ -5,7 +5,7 @@ import {
   platformBrowserDynamicTesting,
 } from "@angular/platform-browser-dynamic/testing";
 import "@testing-library/jest-dom/vitest";
-import { afterAll, afterEach, beforeAll } from "vitest";
+import { afterAll, afterEach, beforeAll, vi } from "vitest";
 import * as auth from "../../some-auth-provider";
 import { server } from "../mocks/index";
 
@@ -13,6 +13,19 @@ getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
 );
+
+/*
+ * Run this `afterEach` first to get back to real timers before any other cleanup.
+ * Use real timers by default.
+ * Individual tests can enable fake timers if they need to.
+ * If they did, run all the pending timers and use real timers.
+ */
+afterEach(async () => {
+  if (vi.isMockFunction(setTimeout)) {
+    await vi.runOnlyPendingTimersAsync();
+    vi.useRealTimers();
+  }
+});
 
 beforeAll(() => {
   server.listen();
