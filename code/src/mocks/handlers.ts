73d3cc0f455d533c3aa221b7a438cba2a87dee64
Dany Reyna <luisd.reyna@outlook.com>
2024-02-13 13:01:27 -0600

    feat(code/src/mocks/handlers.ts): add handler for "login submission" component

--- code/src/mocks/handlers.ts
+++ code/src/mocks/handlers.ts
@@ -1,1 +1,14 @@
-export const handlers = [];
+import { http, HttpResponse, type PathParams } from "msw";
+import type { LoginFormValues } from "../app/login-submission/login-submission-form.component";
+
+export const handlers = [
+  http.post<PathParams, LoginFormValues>(
+    "https://auth-provider.example.com/api/login",
+    async ({ request }) => {
+      const body = await request.json();
+      const { username } = body;
+
+      return HttpResponse.json({ username });
+    },
+  ),
+];
