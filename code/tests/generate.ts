22e26d2ec8b488e35ad7bb50147b66b2b25163dd
Dany Reyna <luisd.reyna@outlook.com>
2024-02-12 20:45:39 -0600

    refactor(code/tests/generate.ts): rename from `generatedAtTest` to `generatedInTest`

--- code/tests/generate.ts
+++ code/tests/generate.ts
@@ -10,7 +10,7 @@ export const buildUser = build<User>({
     source: "registration",
   },
   traits: {
-    generatedAtTest: {
+    generatedInTest: {
       overrides: { source: "test" },
     },
   },
