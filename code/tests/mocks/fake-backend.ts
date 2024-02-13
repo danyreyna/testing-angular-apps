ed376ad0bcabead7b0b1f78127f8f14afa7c4899
Dany Reyna <luisd.reyna@outlook.com>
2024-02-12 20:20:48 -0600

    feat(code/tests/mocks/fake-backend.ts): include `source` property in put users endpoint

--- code/tests/mocks/fake-backend.ts
+++ code/tests/mocks/fake-backend.ts
@@ -11,7 +11,7 @@ type Rfc9457ProblemDetail = {
 
 const fakeUsersDb = new Map<
   string,
-  Pick<User, "username"> & { passwordHash: string }
+  Pick<User, "username" | "source"> & { passwordHash: string }
 >();
 
 function getStringHash(str: string) {
@@ -25,12 +25,12 @@ function getStringHash(str: string) {
 }
 
 export const handlers = [
-  http.put<{ id: string }, Pick<User, "username" | "password">>(
+  http.put<{ id: string }, Pick<User, "username" | "password" | "source">>(
     "https://api.example.com/user/:id",
     async ({ params, request }) => {
       const body = await request.json();
 
-      const { username, password } = body;
+      const { username, password, source } = body;
       const { id } = params;
 
       if (!username) {
@@ -55,23 +55,35 @@ export const handlers = [
         return HttpResponse.json(problemDetail, { status });
       }
 
+      if (!source) {
+        const status = 400;
+        const problemDetail: Rfc9457ProblemDetail = {
+          status,
+          title: "A source is required",
+          detail:
+            "To create or update a user a full representation including the source must be provided",
+        };
+        return HttpResponse.json(problemDetail, { status });
+      }
+
       const passwordHash = getStringHash(password);
 
       const existingUser = fakeUsersDb.get(id);
       if (existingUser === undefined) {
-        fakeUsersDb.set(id, { username, passwordHash });
+        fakeUsersDb.set(id, { username, passwordHash, source });
 
         return new HttpResponse(null, { status: 201 });
       }
 
       const doesUserAlreadyHaveRequestedUpdates =
         username === existingUser.username &&
-        passwordHash === existingUser.passwordHash;
+        passwordHash === existingUser.passwordHash &&
+        source === existingUser.source;
       if (doesUserAlreadyHaveRequestedUpdates) {
         return new HttpResponse(null, { status: 200 });
       }
 
-      fakeUsersDb.set(id, { username, passwordHash });
+      fakeUsersDb.set(id, { username, passwordHash, source });
       return new HttpResponse(null, { status: 200 });
     },
   ),
