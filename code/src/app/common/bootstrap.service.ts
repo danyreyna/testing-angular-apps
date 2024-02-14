caae3e8f061b5c0c336669f9f159d26a0a1c8b92
Dany Reyna <luisd.reyna@outlook.com>
2024-02-13 22:39:52 -0600

    feat(code/src/app/common/bootstrap.service.ts): map to include books map

--- code/src/app/common/bootstrap.service.ts
+++ code/src/app/common/bootstrap.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
-import { BehaviorSubject, catchError, mergeMap, shareReplay } from "rxjs";
+import { BehaviorSubject, catchError, map, mergeMap, shareReplay } from "rxjs";
 import type { Book } from "./book.service";
 import { handleObservableError } from "./handle-observable-error";
 import type { ListItem } from "./list-item.service";
@@ -26,6 +26,18 @@ export class BootstrapService {
   );
 
   bootstrapData$ = this.#bootstrapResponse$.pipe(
+    map(({ user, listItems }) => {
+      const books = listItems.reduce((accumulator, { book }) => {
+        if (book === null) {
+          return accumulator;
+        }
+
+        accumulator.set(book.id, book);
+        return accumulator;
+      }, new Map<string, Book>());
+
+      return { user, listItems, books };
+    }),
     shareReplay(1),
     catchError((errorResponse) => handleObservableError(errorResponse)),
   );
