c0481cf10717912cf6dbccb8adf921709551111c
Dany Reyna <luisd.reyna@outlook.com>
2024-02-13 18:40:13 -0600

    refactor(code/src/mocks): rename from `handlers.ts` to `some-test-api.ts`

--- code/src/mocks/browser.ts
+++ code/src/mocks/browser.ts
@@ -1,4 +1,4 @@
 import { setupWorker } from "msw/browser";
-import { handlers } from "./handlers";
+import { handlers as someTestApiHandlers } from "./some-test-api";
 
-export const worker = setupWorker(...handlers);
+export const worker = setupWorker(...someTestApiHandlers);
