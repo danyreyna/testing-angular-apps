fe19a0dddfac52ae7474efe275337ccaa4b812b2
Dany Reyna <luisd.reyna@outlook.com>
2024-02-15 01:14:54 -0600

    feat(code/tests/mocks/app-handlers.ts): simulate a small network delay

--- code/tests/mocks/app-handlers.ts
+++ code/tests/mocks/app-handlers.ts
@@ -10,63 +10,72 @@ import { mockUserDbTable } from "./user-handlers";
 
 export const handlers = [
   // endpoint to get the user's information and preload data in the in-memory cache
-  http.get<PathParams>("https://api.example.com/bootstrap", ({ cookies }) => {
-    const sessionId = cookies["__Host-id"];
-    if (sessionId === undefined) {
-      const status = 401;
-      return HttpResponse.json<Rfc9457ProblemDetail>(
-        {
-          status,
-          title: "A token must be provided",
-          detail: "Can't bootstrap the application without a token",
-        },
-        { status },
-      );
-    }
+  http.get<PathParams>(
+    "https://api.example.com/bootstrap",
+    async ({ cookies }) => {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(null);
+        }, Math.random() * 3_000);
+      });
 
-    const session = mockSessionDbTable.get(sessionId);
-    if (session === undefined) {
-      const status = 401;
-      return HttpResponse.json<Rfc9457ProblemDetail>(
-        {
-          status,
-          title: "Invalid token. Please login again.",
-          detail: "Can't bootstrap the application without a valid token",
-        },
-        { status },
-      );
-    }
+      const sessionId = cookies["__Host-id"];
+      if (sessionId === undefined) {
+        const status = 401;
+        return HttpResponse.json<Rfc9457ProblemDetail>(
+          {
+            status,
+            title: "A token must be provided",
+            detail: "Can't bootstrap the application without a token",
+          },
+          { status },
+        );
+      }
 
-    const user = mockUserDbTable.get(session.userId);
-    if (user === undefined) {
-      const status = 404;
-      return HttpResponse.json<Rfc9457ProblemDetail>(
-        {
-          status,
-          title: "User not found",
-          detail: "Can't find an user with the provided token",
-        },
-        { status },
-      );
-    }
+      const session = mockSessionDbTable.get(sessionId);
+      if (session === undefined) {
+        const status = 401;
+        return HttpResponse.json<Rfc9457ProblemDetail>(
+          {
+            status,
+            title: "Invalid token. Please login again.",
+            detail: "Can't bootstrap the application without a valid token",
+          },
+          { status },
+        );
+      }
 
-    const { id, username, source } = user;
+      const user = mockUserDbTable.get(session.userId);
+      if (user === undefined) {
+        const status = 404;
+        return HttpResponse.json<Rfc9457ProblemDetail>(
+          {
+            status,
+            title: "User not found",
+            detail: "Can't find an user with the provided token",
+          },
+          { status },
+        );
+      }
 
-    const userWithoutPassword: UserWithoutPassword = {
-      id,
-      username,
-      source,
-    };
+      const { id, username, source } = user;
 
-    const listItems = getListItemsByOwner(userWithoutPassword.id);
-    const listItemsAndBooks = listItems.map((listItem) => ({
-      ...listItem,
-      book: mockBookDbTable.get(listItem.bookId) ?? null,
-    }));
+      const userWithoutPassword: UserWithoutPassword = {
+        id,
+        username,
+        source,
+      };
 
-    return HttpResponse.json<BootstrapData>({
-      user: userWithoutPassword,
-      listItems: listItemsAndBooks,
-    });
-  }),
+      const listItems = getListItemsByOwner(userWithoutPassword.id);
+      const listItemsAndBooks = listItems.map((listItem) => ({
+        ...listItem,
+        book: mockBookDbTable.get(listItem.bookId) ?? null,
+      }));
+
+      return HttpResponse.json<BootstrapData>({
+        user: userWithoutPassword,
+        listItems: listItemsAndBooks,
+      });
+    },
+  ),
 ];
