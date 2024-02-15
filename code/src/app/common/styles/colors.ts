010e8b22682b1b5dd78c97897fb7b2f24cc45712
Dany Reyna <luisd.reyna@outlook.com>
2024-02-14 23:02:58 -0600

    feat(code/src/app/common/styles): add `colors.ts`

---
+++ code/src/app/common/styles/colors.ts
@@ -0,0 +1,13 @@
+export const BASE_COLOR = "white";
+export const TEXT_COLOR = "#434449";
+export const GRAY_COLOR = "#f1f2f7";
+export const GRAY_10_COLOR = "#f1f1f4";
+export const GRAY_20_COLOR = "#e4e5e9";
+export const GRAY_80_COLOR = "#6f7077";
+export const INDIGO_COLOR = "#3f51b5";
+export const INDIGO_DARKEN_10_COLOR = "#364495";
+export const INDIGO_LIGHTEN_80_COLOR = "#b7c1f8";
+export const YELLOW_COLOR = "#ffc107";
+export const GREEN_COLOR = "#4caf50";
+export const DANGER_COLOR = "#ef5350";
+export const ORANGE_COLOR = "orange";
