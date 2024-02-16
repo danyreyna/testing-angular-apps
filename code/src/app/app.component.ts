134b00a9f5008831e912a1efc985eebb6ad9db88
Dany Reyna <luisd.reyna@outlook.com>
2024-02-15 23:31:16 -0600

    feat(code/src/app): dynamically import the unauthenticated or authenticated app

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -1,16 +1,39 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  type OnInit,
+  type Type,
+} from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { AuthComponent } from "./common/auth/auth.component";
+import { AuthService } from "./common/auth/auth.service";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AuthComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <router-outlet />
+    <app-auth>
+      <ng-container *ngComponentOutlet="componentClass"></ng-container>
+    </app-auth>
   `,
 })
-export class AppComponent {
-  protected readonly title = "testing-angular-apps";
+export class AppComponent implements OnInit {
+  readonly #authService = inject(AuthService);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  protected componentClass: null | Type<any> = null;
+
+  async ngOnInit() {
+    if (this.#authService.user() === null) {
+      const module = await import("./unauthenticated-app.component");
+      this.componentClass = module.UnauthenticatedAppComponent;
+    } else {
+      const module = await import("./authenticated-app.component");
+      this.componentClass = module.AuthenticatedAppComponent;
+    }
+  }
 }
