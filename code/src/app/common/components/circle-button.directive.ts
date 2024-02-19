58996f5a4757193cbe039068ff47f3c65848d028
Dany Reyna <luisd.reyna@outlook.com>
2024-02-18 23:36:08 -0600

    refactor(code/src/app/common/components): convert circle-button component into a directive

---
+++ code/src/app/common/components/circle-button.directive.ts
@@ -0,0 +1,52 @@
+import {
+  Directive,
+  ElementRef,
+  inject,
+  Input,
+  type OnInit,
+} from "@angular/core";
+
+@Directive({
+  selector: "[appCircleButton]",
+  standalone: true,
+})
+export class CircleButtonDirective implements OnInit {
+  readonly #element = inject<ElementRef<HTMLButtonElement>>(ElementRef);
+
+  @Input({ required: true })
+  textLabel = "";
+
+  ngOnInit() {
+    /*
+     * This is ugly, but with an `app-circle-button` component we'd end up with HTML like this:
+     * ```html
+     * <app-circle-button (buttonClicked)="someClickHandler()">
+     *   <button (click)="theInternalClickHandler($event)">
+     *     <div class="screen-reader-only">Close</div>
+     *     <span aria-hidden="true">×</span>
+     *   </button>
+     *  </app-circle-button>
+     * ```
+     * So much for an "enterprise" framework.
+     */
+
+    this.#element.nativeElement.classList.add("app-circle-button");
+
+    const textLabelNode = document.createTextNode(this.textLabel);
+    const textLabelDiv = document.createElement("div");
+    textLabelDiv.classList.add("screen-reader-only");
+    textLabelDiv.appendChild(textLabelNode);
+
+    const originalButtonContent = Array.from(
+      this.#element.nativeElement.childNodes,
+    );
+    this.#element.nativeElement.textContent = "";
+
+    const buttonContentSpan = document.createElement("span");
+    buttonContentSpan.setAttribute("aria-hidden", "true");
+    buttonContentSpan.append(...originalButtonContent);
+
+    this.#element.nativeElement.appendChild(textLabelDiv);
+    this.#element.nativeElement.appendChild(buttonContentSpan);
+  }
+}
