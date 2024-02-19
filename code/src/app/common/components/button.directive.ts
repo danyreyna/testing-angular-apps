1642c6fc14057dd694ba933ac0a76ee4dcef55c0
Dany Reyna <luisd.reyna@outlook.com>
2024-02-18 22:25:49 -0600

    refactor(code/src/app/common/components/button.directive.ts): use shared class

--- code/src/app/common/components/button.directive.ts
+++ code/src/app/common/components/button.directive.ts
@@ -1,10 +1,4 @@
 import { Directive, ElementRef, inject, Input } from "@angular/core";
-import {
-  BASE_COLOR,
-  GRAY_COLOR,
-  INDIGO_COLOR,
-  TEXT_COLOR,
-} from "../styles/colors";
 
 export type ButtonVariant = "primary" | "secondary";
 
@@ -29,20 +23,14 @@ export class ButtonDirective {
      * That's uglier.
      */
 
-    this.#element.nativeElement.style.cursor = "pointer";
-    this.#element.nativeElement.style.padding = "10px 15px";
-    this.#element.nativeElement.style.border = "0";
-    this.#element.nativeElement.style.lineHeight = "1";
-    this.#element.nativeElement.style.borderRadius = "3px";
+    this.#element.nativeElement.classList.add("app-button");
 
     switch (this.variant) {
       case "primary":
-        this.#element.nativeElement.style.background = INDIGO_COLOR;
-        this.#element.nativeElement.style.color = BASE_COLOR;
+        this.#element.nativeElement.classList.add("app-button--primary");
         break;
       case "secondary":
-        this.#element.nativeElement.style.background = GRAY_COLOR;
-        this.#element.nativeElement.style.color = TEXT_COLOR;
+        this.#element.nativeElement.classList.add("app-button--secondary");
         break;
       default: {
         const exhaustiveCheck: never = this.variant;
