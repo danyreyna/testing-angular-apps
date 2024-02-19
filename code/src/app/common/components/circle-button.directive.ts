16d587cd6db1bf4f42c36bc49fbf2ff6e879c426
Dany Reyna <luisd.reyna@outlook.com>
2024-02-19 00:13:14 -0600

    feat(code/src/app/common/components/circle-button.directive.ts): add width and height inputs

--- code/src/app/common/components/circle-button.directive.ts
+++ code/src/app/common/components/circle-button.directive.ts
@@ -16,6 +16,12 @@ export class CircleButtonDirective implements OnInit {
   @Input({ required: true })
   textLabel = "";
 
+  @Input()
+  width = 40;
+
+  @Input()
+  height = 40;
+
   ngOnInit() {
     /*
      * This is ugly, but with an `app-circle-button` component we'd end up with HTML like this:
@@ -31,6 +37,8 @@ export class CircleButtonDirective implements OnInit {
      */
 
     this.#element.nativeElement.classList.add("app-circle-button");
+    this.#element.nativeElement.style.width = `${this.width}px`;
+    this.#element.nativeElement.style.height = `${this.height}px`;
 
     const textLabelNode = document.createTextNode(this.textLabel);
     const textLabelDiv = document.createElement("div");
