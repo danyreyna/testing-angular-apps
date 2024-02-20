496158a70daa5256e9cb232dc79888de462530b9
Dany Reyna <luisd.reyna@outlook.com>
2024-02-19 19:27:46 -0600

    ref(code/src/app/common/components): convert input directive into a component

---
+++ code/src/app/common/components/input.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+  selector: "input[app-input]",
+  standalone: true,
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: `
+      :host {
+        border-radius: 3px;
+        border: 1px solid #f1f1f4;
+        background: #f1f2f7;
+        padding: 8px 12px;
+      }
+    `,
+  template: ``,
+})
+export class InputComponent {}
