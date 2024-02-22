5f5174910a74952692bdf3568d5223e55dd0d853
Dany Reyna <luisd.reyna@outlook.com>
2024-02-22 00:10:42 -0600

    feat(code/src/app/common/components): add error message

---
+++ code/src/app/common/components/error-message.component.ts
@@ -0,0 +1,53 @@
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { DANGER_COLOR } from "../styles/colors";
+
+export type ErrorMessageVariant = "stacked" | "inline";
+
+@Component({
+  selector: "app-error-message",
+  standalone: true,
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  /*
+   * Go and complain to the Angular team.
+   * https://github.com/angular/angular/issues/53809
+   */
+  // eslint-disable-next-line @angular-eslint/no-host-metadata-property
+  host: {
+    role: "alert",
+  },
+  styles: [
+    `
+      :host {
+        color: ${DANGER_COLOR};
+      }
+
+      .message {
+        white-space: break-spaces;
+        margin: 0 0 -5px;
+      }
+
+      :host[variant="stacked"],
+      .message[variant="stacked"] {
+        display: block;
+      }
+
+      :host[variant="inline"],
+      .message[variant="inline"] {
+        display: inline-block;
+      }
+    `,
+  ],
+  template: `
+    <span>There was an error:</span>
+    <pre class="message">{{ errorMessage }}</pre>
+  `,
+})
+export class ErrorMessageComponent {
+  @Input({ required: true })
+  errorMessage = "";
+
+  @Input()
+  variant: ErrorMessageVariant = "stacked";
+}
