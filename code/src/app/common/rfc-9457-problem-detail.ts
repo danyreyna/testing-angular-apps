811e9df6c843d1593b0b4722011fe59001d7f9e9
Dany Reyna <luisd.reyna@outlook.com>
2024-02-25 19:46:02 -0600

    feat(code/src/app/common/rfc-9457-problem-detail.ts): add `isRfc9457ProblemDetail` and `isRequiredPropertiesProblemDetail`

--- code/src/app/common/rfc-9457-problem-detail.ts
+++ code/src/app/common/rfc-9457-problem-detail.ts
@@ -1,3 +1,5 @@
+import { isObjectLike, type ObjectLike } from "./is-object-like";
+
 export type Rfc9457ProblemDetail = {
   type?: string;
   status?: number;
@@ -6,6 +8,22 @@ export type Rfc9457ProblemDetail = {
   instance?: string;
 };
 
+export function isRfc9457ProblemDetail(
+  value: unknown,
+): value is Rfc9457ProblemDetail {
+  if (!isObjectLike(value)) {
+    return false;
+  }
+
+  return (
+    typeof value["type"] === "string" ||
+    typeof value["status"] === "number" ||
+    typeof value["title"] === "string" ||
+    typeof value["detail"] === "string" ||
+    typeof value["instance"] === "string"
+  );
+}
+
 /*
  * Add an "errors" extension to the Problem Detail
  * https://www.rfc-editor.org/rfc/rfc9457.html#name-extension-members
@@ -13,3 +31,24 @@ export type Rfc9457ProblemDetail = {
 export type RequiredPropertiesProblemDetail = Rfc9457ProblemDetail & {
   errors: { title: string; detail: string }[];
 };
+
+function hasErrorsExtension(value: ObjectLike) {
+  const errors = value["errors"];
+
+  if (!Array.isArray(errors)) {
+    return false;
+  }
+
+  return errors.every(
+    (error) =>
+      isObjectLike(error) &&
+      typeof error["title"] === "string" &&
+      typeof error["detail"] === "string",
+  );
+}
+
+export function isRequiredPropertiesProblemDetail(
+  value: unknown,
+): value is RequiredPropertiesProblemDetail {
+  return isRfc9457ProblemDetail(value) && hasErrorsExtension(value);
+}
