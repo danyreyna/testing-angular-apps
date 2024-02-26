1b2fcfa39b2bb28872f7034a7aa112244283ade7
Dany Reyna <luisd.reyna@outlook.com>
2024-02-26 17:39:38 -0600

    feat(code/src): run `timeChangeDetection` every 5 seconds to send performance record to `/profile` endpoint

--- code/src/profiler.ts
+++ code/src/profiler.ts
@@ -44,3 +44,43 @@ export class AngularProfiler {
     return { msPerTick, numTicks };
   }
 }
+
+let profilerInstance: null | AngularProfiler = null;
+
+export function setProfilerInstance(instance: AngularProfiler) {
+  profilerInstance = instance;
+}
+
+export type SendToMonitoringTool = (
+  changeDetectionPerfRecord: ChangeDetectionPerfRecord,
+) => void;
+
+export function startPerformanceMonitor(
+  interval: number,
+  sendToMonitoringTool: SendToMonitoringTool,
+) {
+  setInterval(() => {
+    if (profilerInstance === null) {
+      return;
+    }
+
+    /*
+     * Unlike the React Profiler's `onRender` callback,
+     * Angular doesn't provide a straightforward way to profile everytime components update.
+     * So we'll call `AngularProfiler.timeChangeDetection()` here.
+     *
+     * https://angular.dev/guide/components/lifecycle
+     * And before you think of using a lifecycle hook, keep in mind that:
+     *   - `ngOnChanges` only runs after inputs change.
+     *   - `ngDoCheck` runs very frequently and can significantly impact your page's performance.
+     *     - Avoid defining this hook whenever possible, only using it when you have no alternative.
+     *   - Calling `AngularProfiler.timeChangeDetection()` in `afterRender` is NOT the way to go.
+     *     - A render will trigger change detection, that change detection will trigger at least one render, that render will trigger change detection...
+     *     - This results in `ApplicationRef.tick()` throwing "ApplicationRef.tick is called recursively"
+     *     - And even if you catch and bypass this error, this approach slows down the app.
+     */
+    const changeDetectionPerfRecord = profilerInstance.timeChangeDetection();
+
+    sendToMonitoringTool(changeDetectionPerfRecord);
+  }, interval);
+}
