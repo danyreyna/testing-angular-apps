923f8a5ac9727caded653092011289c777f0c1ce
Dany Reyna <luisd.reyna@outlook.com>
2024-02-25 21:30:53 -0600

    fix(code/tests/mocks/app-handlers.ts): correctly handle session cookies
    Respond with the appropriate CORS headers to handle cookie credentials.
    Persist users and sessions.

--- code/tests/mocks/app-handlers.ts
+++ code/tests/mocks/app-handlers.ts
@@ -1,11 +1,14 @@
 import { delay, http, HttpResponse, type PathParams } from "msw";
 import type { BootstrapResponse } from "../../src/app/common/bootstrap.service";
+import { type Rfc9457ProblemDetail } from "../../src/app/common/rfc-9457-problem-detail";
 import type { UserWithoutPassword } from "../../src/app/common/user";
-import { mockSessionDbTable } from "./auth-handlers";
+import { AUTH_SESSION_COOKIE_NAME } from "./auth/auth-session";
+import { getAuthSession } from "./auth/auth-session-db";
 import { mockBookDbTable } from "./book-handlers";
+import { CORS_HEADERS } from "./common/cors-headers";
+import { handleInternalServerError } from "./common/handle-internal-server-error";
 import { getListItemsByOwner } from "./list-item-handlers";
-import type { Rfc9457ProblemDetail } from "./rfc-9457-problem-detail";
-import { mockUserDbTable } from "./user-handlers";
+import { getUser } from "./user/user-db";
 
 export const handlers = [
   // endpoint to get the user's information and preload data in the in-memory cache
@@ -14,8 +17,8 @@ export const handlers = [
     async ({ cookies }) => {
       await delay();
 
-      const sessionId = cookies["__Host-id"];
-      if (sessionId === undefined) {
+      const authSessionId = cookies[AUTH_SESSION_COOKIE_NAME];
+      if (authSessionId === undefined) {
         const status = 401;
         return HttpResponse.json<Rfc9457ProblemDetail>(
           {
@@ -23,12 +26,19 @@ export const handlers = [
             title: "A token must be provided",
             detail: "Can't bootstrap the application without a token",
           },
-          { status },
+          {
+            status,
+            headers: CORS_HEADERS,
+          },
         );
       }
 
-      const session = mockSessionDbTable.get(sessionId);
-      if (session === undefined) {
+      const authSessionResult = await getAuthSession(authSessionId);
+      if (authSessionResult instanceof Error) {
+        return handleInternalServerError(authSessionResult, CORS_HEADERS);
+      }
+
+      if (authSessionResult === undefined) {
         const status = 401;
         return HttpResponse.json<Rfc9457ProblemDetail>(
           {
@@ -36,12 +46,19 @@ export const handlers = [
             title: "Invalid token. Please login again.",
             detail: "Can't bootstrap the application without a valid token",
           },
-          { status },
+          {
+            status,
+            headers: CORS_HEADERS,
+          },
         );
       }
 
-      const user = mockUserDbTable.get(session.userId);
-      if (user === undefined) {
+      const userResult = await getUser(authSessionResult.userId);
+      if (userResult instanceof Error) {
+        return handleInternalServerError(userResult, CORS_HEADERS);
+      }
+
+      if (userResult === undefined) {
         const status = 404;
         return HttpResponse.json<Rfc9457ProblemDetail>(
           {
@@ -49,11 +66,14 @@ export const handlers = [
             title: "User not found",
             detail: "Can't find an user with the provided token",
           },
-          { status },
+          {
+            status,
+            headers: CORS_HEADERS,
+          },
         );
       }
 
-      const { id, username, source } = user;
+      const { id, username, source } = userResult;
 
       const userWithoutPassword: UserWithoutPassword = {
         id,
@@ -67,10 +87,15 @@ export const handlers = [
         book: mockBookDbTable.get(listItem.bookId) ?? null,
       }));
 
-      return HttpResponse.json<BootstrapResponse>({
-        user: userWithoutPassword,
-        listItems: listItemsAndBooks,
-      });
+      return HttpResponse.json<BootstrapResponse>(
+        {
+          user: userWithoutPassword,
+          listItems: listItemsAndBooks,
+        },
+        {
+          headers: CORS_HEADERS,
+        },
+      );
     },
   ),
 ];
