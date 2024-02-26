d5ea4145caae21ff13ff3bc2cb6f9a2296205948
Dany Reyna <luisd.reyna@outlook.com>
2024-02-25 20:10:24 -0600

    feat(code/src/app/common/handle-observable-error.ts): get error message from `RequiredPropertiesProblemDetail`

--- code/src/app/common/handle-observable-error.ts
+++ code/src/app/common/handle-observable-error.ts
@@ -1,19 +1,38 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import { throwError } from "rxjs";
+import {
+  isRequiredPropertiesProblemDetail,
+  isRfc9457ProblemDetail,
+  type RequiredPropertiesProblemDetail,
+  type Rfc9457ProblemDetail,
+} from "./rfc-9457-problem-detail";
 
 export type HandledHttpError = { message: string; status: number };
 export type HandledObservableError = HandledHttpError | { message: string };
 
+function getProblemDetailMessage(error: Rfc9457ProblemDetail) {
+  return `${error.title ?? ""}${
+    error.detail === undefined ? "" : `: ${error.detail}`
+  }`;
+}
+
+function getRequiredPropertiesProblemDetailMessage(
+  error: RequiredPropertiesProblemDetail,
+) {
+  return `${getProblemDetailMessage(error)}\n${error.errors
+    .map(({ title, detail }) => `${title}: ${detail}`)
+    .join("\n")}`;
+}
+
 function getHttpErrorMessage(errorResponse: HttpErrorResponse) {
-  if (
-    errorResponse.error?.title !== undefined ||
-    errorResponse.error?.detail !== undefined
-  ) {
-    return `${errorResponse.error.title ?? ""}${
-      errorResponse.error.detail === undefined
-        ? ""
-        : `: ${errorResponse.error.detail}`
-    }`;
+  const { error } = errorResponse;
+
+  if (isRequiredPropertiesProblemDetail(error)) {
+    return getRequiredPropertiesProblemDetailMessage(error);
+  }
+
+  if (isRfc9457ProblemDetail(error)) {
+    return getProblemDetailMessage(error);
   }
 
   if (errorResponse.error?.message !== undefined) {
