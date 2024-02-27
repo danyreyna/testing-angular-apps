c42330e8ee73c449f53747b2ad442dadbfc52453
Dany Reyna <luisd.reyna@outlook.com>
2024-02-26 21:54:31 -0600

    feat(code/tests/mocks): add `"https://api.example.com/profiler"` mock handler

--- code/tests/mocks/app-handlers.ts
+++ code/tests/mocks/app-handlers.ts
@@ -1,9 +1,19 @@
 import { delay, http, HttpResponse, type PathParams } from "msw";
 import type { BootstrapResponse } from "../../src/app/common/bootstrap.service";
+import {
+  fetchResponse,
+  parseResponseBody,
+} from "../../src/app/common/fetch-utils";
+import { objectToFormData } from "../../src/app/common/object-to-form-data";
 import { type Rfc9457ProblemDetail } from "../../src/app/common/rfc-9457-problem-detail";
 import type { UserWithoutPassword } from "../../src/app/common/user";
+import type { ChangeDetectionPerfRecord } from "../../src/profiler";
 import { AUTH_SESSION_COOKIE_NAME } from "./auth/auth-session";
 import { getAuthSession } from "./auth/auth-session-db";
+import {
+  buildClientCredentialsFlowRequest,
+  type ClientCredentialsFlowResponse,
+} from "./auth/client-credentials-flow";
 import { mockBookDbTable } from "./book-handlers";
 import { CORS_HEADERS } from "./common/cors-headers";
 import { handleInternalServerError } from "./common/handle-internal-server-error";
@@ -96,6 +106,91 @@ export const handlers = [
           headers: CORS_HEADERS,
         },
       );
+    },
+  ),
+  http.post<PathParams, ChangeDetectionPerfRecord>(
+    "https://api.example.com/profiler",
+    async ({ cookies, request }) => {
+      const authSessionId = cookies[AUTH_SESSION_COOKIE_NAME];
+      if (authSessionId === undefined) {
+        const status = 401;
+        return HttpResponse.json<Rfc9457ProblemDetail>(
+          {
+            status,
+            title: "A token must be provided",
+            detail: "Can't profile the application without a token",
+          },
+          {
+            status,
+            headers: CORS_HEADERS,
+          },
+        );
+      }
+
+      /*
+       * In a real backend the `client_id` and `client_secret`
+       * would be stored in a secrets manager.
+       */
+      const clientCredentialsFlowRequest = buildClientCredentialsFlowRequest();
+
+      const monitoringServiceTokenResponse = await fetchResponse(() =>
+        fetch("https://some-monitoring-service.com/oauth/token", {
+          method: "post",
+          body: objectToFormData(clientCredentialsFlowRequest),
+        }),
+      );
+      if (monitoringServiceTokenResponse instanceof Error) {
+        return handleInternalServerError(
+          monitoringServiceTokenResponse,
+          CORS_HEADERS,
+        );
+      }
+      if (!monitoringServiceTokenResponse.ok) {
+        return handleInternalServerError(
+          new Error(
+            `The monitoring service token response has status "${monitoringServiceTokenResponse.status}"`,
+          ),
+          CORS_HEADERS,
+        );
+      }
+      const clientCredentialsFlowResponse =
+        await parseResponseBody<ClientCredentialsFlowResponse>(
+          monitoringServiceTokenResponse,
+        );
+      if (clientCredentialsFlowResponse instanceof Error) {
+        return handleInternalServerError(
+          clientCredentialsFlowResponse,
+          CORS_HEADERS,
+        );
+      }
+
+      const changeDetectionPerfRecord = await request.json();
+
+      const performanceMetricsResponse = await fetchResponse(() =>
+        fetch("https://some-monitoring-service.com/metrics", {
+          method: "post",
+          headers: {
+            Authorization: `Bearer ${clientCredentialsFlowResponse.access_token}`,
+          },
+          body: JSON.stringify(changeDetectionPerfRecord),
+        }),
+      );
+      if (performanceMetricsResponse instanceof Error) {
+        return handleInternalServerError(
+          performanceMetricsResponse,
+          CORS_HEADERS,
+        );
+      }
+      if (!performanceMetricsResponse.ok) {
+        return handleInternalServerError(
+          new Error(
+            `The monitoring service metrics response has status "${performanceMetricsResponse.status}"`,
+          ),
+          CORS_HEADERS,
+        );
+      }
+
+      return new HttpResponse(null, { status: 204 });
     },
   ),
 ];
