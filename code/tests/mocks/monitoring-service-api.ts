f79d07d6436148d66f5bce264fd7781aa3664382
Dany Reyna <luisd.reyna@outlook.com>
2024-02-26 19:53:34 -0600

    feat(code/tests/mocks/monitoring-service-api.ts): add types to handlers

--- code/tests/mocks/monitoring-service-api.ts
+++ code/tests/mocks/monitoring-service-api.ts
@@ -1,29 +1,42 @@
-import { http, HttpResponse } from "msw";
+import {
+  type DefaultBodyType,
+  type DefaultRequestMultipartBody,
+  http,
+  HttpResponse,
+  type PathParams,
+} from "msw";
 import type { Rfc9457ProblemDetail } from "../../src/app/common/rfc-9457-problem-detail";
-import { generateJwt } from "../generate";
+import { generateJwt, type Jwt } from "../generate";
 
 const EXPIRES_24_HOURS = 86400;
 
+export type ClientCredentialsFlowResponse = {
+  access_token: Jwt;
+  token_type: "Bearer";
+  expires_in: number;
+};
+
 export const handlers = [
-  http.post(
-    "https://some-monitoring-service.com/oauth/token",
-    async ({ request }) => {
-      const formData = await request.formData();
+  http.post<
+    PathParams,
+    DefaultRequestMultipartBody,
+    ClientCredentialsFlowResponse
+  >("https://some-monitoring-service.com/oauth/token", async ({ request }) => {
+    const formData = await request.formData();
 
-      console.log("The real third party API should validate these...");
-      console.log(`audience: ${formData.get("audience")}`);
-      console.log(`grant_type: ${formData.get("grant_type")}`);
-      console.log(`client_id: ${formData.get("client_id")}`);
-      console.log(`client_secret: ${formData.get("client_secret")}`);
+    console.log("The real third party API should validate these...");
+    console.log(`audience: ${formData.get("audience")}`);
+    console.log(`grant_type: ${formData.get("grant_type")}`);
+    console.log(`client_id: ${formData.get("client_id")}`);
+    console.log(`client_secret: ${formData.get("client_secret")}`);
 
-      return HttpResponse.json({
-        access_token: generateJwt(),
-        token_type: "Bearer",
-        expires_in: EXPIRES_24_HOURS,
-      });
-    },
-  ),
-  http.post(
+    return HttpResponse.json({
+      access_token: generateJwt(),
+      token_type: "Bearer",
+      expires_in: EXPIRES_24_HOURS,
+    });
+  }),
+  http.post<PathParams, DefaultBodyType>(
     "https://some-monitoring-service.com/metrics",
     async ({ request }) => {
       // The real third party API should validate the bearer token
