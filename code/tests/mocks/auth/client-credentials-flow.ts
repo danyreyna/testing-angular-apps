0a838eec5059415fa229b8ec59509f31094ae633
Dany Reyna <luisd.reyna@outlook.com>
2024-02-26 20:18:24 -0600

    feat(code/tests/mocks/auth/client-credentials-flow.ts): add `ClientCredentialsFlowRequest` and `buildClientCredentialsFlowRequest`

--- code/tests/mocks/auth/client-credentials-flow.ts
+++ code/tests/mocks/auth/client-credentials-flow.ts
@@ -1,3 +1,5 @@
+import { faker } from "@faker-js/faker";
+import { build, perBuild } from "@jackfranklin/test-data-bot";
 import type { Jwt } from "./jwt";
 
 export type ClientCredentialsFlowResponse = {
@@ -5,3 +7,23 @@ export type ClientCredentialsFlowResponse = {
   token_type: "Bearer";
   expires_in: number;
 };
+
+export type ClientCredentialsFlowRequest = {
+  audience: string;
+  grant_type: "client_credentials";
+  client_id: string;
+  client_secret: string;
+};
+
+export const buildClientCredentialsFlowRequest =
+  build<ClientCredentialsFlowRequest>({
+    fields: {
+      audience: "https://api.example.com",
+      grant_type: "client_credentials",
+      client_id: perBuild(() => faker.string.alphanumeric({ length: 32 })),
+      client_secret: faker.string.fromCharacters(
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_",
+        64,
+      ),
+    },
+  });
