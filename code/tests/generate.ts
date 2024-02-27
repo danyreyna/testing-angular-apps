fc7ae04926455c70ed79b4390f050b56ecb1ae12
Dany Reyna <luisd.reyna@outlook.com>
2024-02-26 19:50:04 -0600

    feat(code/tests/generate.ts): export `Jwt` type

--- code/tests/generate.ts
+++ code/tests/generate.ts
@@ -16,7 +16,9 @@ export const buildUser = build<User>({
   },
 });
 
-export function generateJwt(): `${string}.${string}.${string}` {
+export type Jwt = `${string}.${string}.${string}`;
+
+export function generateJwt(): Jwt {
   return `eyJ${faker.string.alphanumeric({ length: 71 })}J9.eyJ${faker.string.alphanumeric({ length: 371 })}J9.${faker.string.fromCharacters(
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-",
     342,
