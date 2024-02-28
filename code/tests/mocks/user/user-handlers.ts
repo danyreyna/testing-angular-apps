82697cf280f25857acca1e12f72822fe6ac55fca
Dany Reyna <luisd.reyna@outlook.com>
2024-02-27 21:03:51 -0600

    feat(code/tests/mocks/user/user-handlers.ts): add `GET https://api.example.com/user/:id` endpoint

--- code/tests/mocks/user/user-handlers.ts
+++ code/tests/mocks/user/user-handlers.ts
@@ -3,9 +3,53 @@ import type { Rfc9457ProblemDetail } from "../../../src/app/common/rfc-9457-prob
 import { AUTH_SESSION_COOKIE_NAME } from "../auth/auth-session";
 import { CORS_HEADERS } from "../common/cors-headers";
 import { handleInternalServerError } from "../common/handle-internal-server-error";
-import { deleteUsersWithSource } from "./user-db";
+import type { DbUser } from "../common/mock-db";
+import { deleteUsersWithSource, getUser } from "./user-db";
 
 export const handlers = [
+  http.get<{ id: string }, DefaultBodyType, Rfc9457ProblemDetail | DbUser>(
+    "https://api.example.com/user/:id",
+    async ({ cookies, params }) => {
+      const authSessionId = cookies[AUTH_SESSION_COOKIE_NAME];
+      if (authSessionId === undefined) {
+        const status = 401;
+        return HttpResponse.json<Rfc9457ProblemDetail>(
+          {
+            status,
+            title: "A token must be provided",
+            detail: "Can't get user information without a token",
+          },
+          {
+            status,
+            headers: CORS_HEADERS,
+          },
+        );
+      }
+
+      const { id } = params;
+      const existingUser = await getUser(id);
+      if (existingUser instanceof Error) {
+        return handleInternalServerError(existingUser, CORS_HEADERS);
+      }
+
+      if (existingUser === undefined) {
+        const status = 404;
+        return HttpResponse.json<Rfc9457ProblemDetail>(
+          {
+            status,
+            title: "User not found",
+            detail: `Can't find a user with the ID "${id}"`,
+          },
+          {
+            status,
+            headers: CORS_HEADERS,
+          },
+        );
+      }
+
+      return HttpResponse.json<DbUser>(existingUser, { headers: CORS_HEADERS });
+    },
+  ),
   http.delete<PathParams, DefaultBodyType, Rfc9457ProblemDetail | undefined>(
     "https://api.example.com/user",
     async ({ cookies, request }) => {
