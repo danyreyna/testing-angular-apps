862929a96738edb6d663a1308d7fdb8d33913897
Dany Reyna <luisd.reyna@outlook.com>
2024-02-28 22:14:11 -0600

    fix(code/src/app/common/response-state/http-res.ts): fix non-nullable body type

--- code/src/app/common/response-state/http-res.ts
+++ code/src/app/common/response-state/http-res.ts
@@ -1,6 +1,8 @@
 import type { HttpResponse } from "@angular/common/http";
 
-export type HttpRes<T> = HttpResponse<T> & { readonly body: T };
+export type HttpRes<T> = HttpResponse<T> & {
+  body: NonNullable<HttpResponse<T>["body"]>;
+};
 
 export function isHttpRes<T>(
   httpResponse: HttpResponse<T>,
