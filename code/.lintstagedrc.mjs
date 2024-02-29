9c499a65dd0255f90cb45a7f91d04a38ba0ba0a8
Dany Reyna <luisd.reyna@outlook.com>
2024-02-28 21:51:16 -0600

    build(lint-staged): revert tsc usage as it compiles the whole project

--- code/.lintstagedrc.mjs
+++ code/.lintstagedrc.mjs
@@ -1,5 +1,4 @@
 const config = {
-  "*.ts": () => "npx tsc --noEmit --project tsconfig.json",
   "*.{js,ts,html}": ["eslint --cache --fix", "prettier --write"],
   "*.{css,scss,md}": "prettier --write",
 };
