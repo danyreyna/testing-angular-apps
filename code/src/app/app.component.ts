f1e30aeaad5ec3d8e225005c431d7ec4d98b2dca
Dany Reyna <luisd.reyna@outlook.com>
2024-03-03 21:56:16 -0600

    feat(code/src/app/app.component.ts): also lazy load `AuthenticatedAppModule`

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { CommonModule } from "@angular/common";
+import { CommonModule, type NgComponentOutlet } from "@angular/common";
 import {
   ChangeDetectionStrategy,
   Component,
   effect,
   inject,
   signal,
-  type Type,
 } from "@angular/core";
 import { AuthComponent } from "./common/auth/auth.component";
 import { AuthService } from "./common/auth/auth.service";
@@ -13,28 +12,46 @@ import { AuthService } from "./common/auth/auth.service";
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [AuthComponent, CommonModule],
+  imports: [CommonModule, AuthComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <app-auth>
-      <ng-container *ngComponentOutlet="componentClass()"></ng-container>
+      <ng-container
+        *ngComponentOutlet="
+          componentOutlet().component;
+          ngModule: componentOutlet().module
+        "
+      ></ng-container>
     </app-auth>
   `,
 })
 export class AppComponent {
   readonly #authService = inject(AuthService);
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  protected componentClass = signal<null | Type<any>>(null);
+  protected componentOutlet = signal<{
+    module?: NgComponentOutlet["ngComponentOutletNgModule"];
+    component: NgComponentOutlet["ngComponentOutlet"];
+  }>({
+    component: null,
+  });
 
   constructor() {
     effect(async () => {
       if (this.#authService.user() === null) {
-        const module = await import("./unauthenticated-app.component");
-        this.componentClass.set(module.UnauthenticatedAppComponent);
+        const esModule = await import("./unauthenticated-app.component");
+
+        this.componentOutlet.set({
+          module: undefined,
+          component: esModule.UnauthenticatedAppComponent,
+        });
       } else {
-        const module = await import("./authenticated-app.component");
-        this.componentClass.set(module.AuthenticatedAppComponent);
+        const ngModuleEsModule = await import("./authenticated-app.module");
+        const componentEsModule = await import("./authenticated-app.component");
+
+        this.componentOutlet.set({
+          module: ngModuleEsModule.AuthenticatedAppModule,
+          component: componentEsModule.AuthenticatedAppComponent,
+        });
       }
     });
   }
