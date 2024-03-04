cf369ab08e3bddbead26402b8a0eaed4966cd38d
Dany Reyna <luisd.reyna@outlook.com>
2024-03-03 21:15:05 -0600

    feat(code/src/app/authenticated-app.component.ts): add error boundary and user bar with logout button, and convert to a module dependant component

--- code/src/app/authenticated-app.component.ts
+++ code/src/app/authenticated-app.component.ts
@@ -1,5 +1,13 @@
-import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ErrorHandler,
+  inject,
+  type OnDestroy,
+} from "@angular/core";
+import type { Subscription } from "rxjs";
+import { AuthService } from "./common/auth/auth.service";
+import type { ErrorBoundaryHandler } from "./common/error/error-boundary";
 import { startPerformanceMonitor } from "./common/profiler";
 
 startPerformanceMonitor(10_000, (changeDetectionPerfRecord) => {
@@ -12,12 +20,56 @@ startPerformanceMonitor(10_000, (changeDetectionPerfRecord) => {
 
 @Component({
   selector: "app-authenticated-app",
-  standalone: true,
-  imports: [CommonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  styles: [``],
+  styles: [
+    `
+      .user-bar {
+        display: flex;
+        align-items: center;
+        position: absolute;
+        top: 10px;
+        right: 10px;
+      }
+
+      .button {
+        margin-left: 10px;
+      }
+    `,
+  ],
   template: `
-    <div>auth</div>
+    <app-error-boundary>
+      <app-full-page-error-fallback
+        fallback
+        [errorMessage]="errorBoundaryHandler.error()?.message ?? ''"
+      />
+
+      <div class="user-bar">
+        {{ authService.user()?.username }}
+        <button
+          app-button
+          variant="secondary"
+          class="button"
+          (click)="authService.logoutSubject.next('logout')"
+        >
+          Logout
+        </button>
+      </div>
+    </app-error-boundary>
   `,
 })
-export class AuthenticatedAppComponent {}
+export class AuthenticatedAppComponent implements OnDestroy {
+  protected readonly errorBoundaryHandler = inject(
+    ErrorHandler,
+  ) as ErrorBoundaryHandler;
+  protected readonly authService = inject(AuthService);
+
+  #logoutSubscription: null | Subscription = null;
+
+  constructor() {
+    this.#logoutSubscription = this.authService.logout$.subscribe();
+  }
+
+  ngOnDestroy() {
+    this.#logoutSubscription?.unsubscribe();
+  }
+}
