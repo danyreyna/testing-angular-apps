33c843731f7f77263a8d7cec01c708b0db7ab912
Dany Reyna <luisd.reyna@outlook.com>
2024-03-04 19:14:00 -0600

    fix(code/src/app/app-routes.module.ts): class name

--- code/src/app/app-routes.module.ts
+++ code/src/app/app-routes.module.ts
@@ -12,4 +12,4 @@ import { ErrorBoundaryHandler } from "./common/error/error-boundary";
   providers: [{ provide: ErrorHandler, useClass: ErrorBoundaryHandler }],
   exports: [AppRoutesComponent],
 })
-export class AuthenticatedAppModule {}
+export class AppRoutesModule {}
