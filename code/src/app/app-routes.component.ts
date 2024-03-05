d260b6aff123d96ab24fe332c0c682acdf3879f7
Dany Reyna <luisd.reyna@outlook.com>
2024-03-04 19:17:11 -0600

    feat(code/src/app): add error boundary to app routes

--- code/src/app/app-routes.component.ts
+++ code/src/app/app-routes.component.ts
@@ -1,10 +1,44 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ErrorHandler,
+  inject,
+  Input,
+} from "@angular/core";
+import type { ErrorBoundaryHandler } from "./common/error/error-boundary";
+import { ErrorMessageComponent } from "./common/error/error-message.component";
+
+@Component({
+  selector: "app-error-fallback",
+  standalone: true,
+  imports: [CommonModule, ErrorMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <app-error-message [errorMessage]="errorMessage" />
+  `,
+})
+export class ErrorFallbackComponent {
+  @Input({ required: true })
+  errorMessage = "";
+}
 
 @Component({
   selector: "[app-routes]",
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <router-outlet />
+    <ng-container app-error-boundary>
+      <app-error-fallback
+        [errorMessage]="errorBoundaryHandler.error()?.message ?? ''"
+        fallback
+      />
+
+      <router-outlet />
+    </ng-container>
   `,
 })
-export class AppRoutesComponent {}
+export class AppRoutesComponent {
+  protected readonly errorBoundaryHandler = inject(
+    ErrorHandler,
+  ) as ErrorBoundaryHandler;
+}
