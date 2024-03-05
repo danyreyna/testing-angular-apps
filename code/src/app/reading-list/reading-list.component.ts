f4b9da87c6ceab85fdacfbeab7ed48d699467fc2
Dany Reyna <luisd.reyna@outlook.com>
2024-03-04 18:17:50 -0600

    feat(code/src/app): add `reading-list/reading-list.component.ts`

---
+++ code/src/app/reading-list/reading-list.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from "@angular/common";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+  selector: "app-reading-list",
+  standalone: true,
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: [``],
+  template: ``,
+})
+export class ReadingListComponent {}
