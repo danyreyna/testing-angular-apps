5e22057b9bb283b73b42f2aad060a45049e6437a
Dany Reyna <luisd.reyna@outlook.com>
2024-03-16 16:41:25 -0600

    fix(code/src/app/common/bootstrap.service.ts): fix list item type

--- code/src/app/common/bootstrap.service.ts
+++ code/src/app/common/bootstrap.service.ts
@@ -10,7 +10,7 @@ import type { UserWithoutPassword } from "./user";
 
 export type BootstrapResponse = {
   user: UserWithoutPassword;
-  listItems: (ListItem & { book: null | Book })[];
+  listItems: (Omit<ListItem, "book"> & { book: null | Book })[];
 };
 
 type NoAuthData = null;
